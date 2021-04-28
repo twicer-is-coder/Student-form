7e3bb8308210012bc3208143ceb846b25d89f013
Saad Nadeem <xaad46@gmail.com>
2021-04-28 23:52:42 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server,{
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 const PORT = process.env.PORT || 3030;
 
 io.on('connection', (socket) => {
