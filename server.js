7b9fc8f8b7c2be02c376a13aa7b05e2d9b063b9f
Saad Nadeem <xaad46@gmail.com>
2021-04-29 00:38:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,41 +3,46 @@ const app = express();
 var cors = require('cors');
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 const PORT = process.env.PORT || 3030;
 
 app.use(cors());
 
 io.on('connection', (socket) => {
-   
+
     console.log('A User Connected');
     socket.broadcast.emit("New User");
 
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected',userId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
     });
-  
+
     socket.on('disconnect', () => {
         console.log('A User Disconnected');
     });
 
-    socket.on('VideoPlayPause', function(pause){
-        socket.broadcast.emit('RecieveVid',pause);
+    socket.on('VideoPlayPause', function (pause) {
+        socket.broadcast.emit('RecieveVid', pause);
     });
 
-    socket.on('BarTimeUpdate', function(Time){
-        socket.broadcast.emit('BarTimeUpdate',Time);
+    socket.on('BarTimeUpdate', function (Time) {
+        socket.broadcast.emit('BarTimeUpdate', Time);
     });
 
     socket.on('Chat Message', (Message) => {
-      // io.emit('LatestMessage', msg);
-      console.log("Message",Message);
-      socket.broadcast.emit('Chat Message', {"Message":Message});
+        // io.emit('LatestMessage', msg);
+        console.log("Message", Message);
+        socket.broadcast.emit('Chat Message', { "Message": Message });
     });
 
 });
 
 server.listen(PORT, () => {
-  console.log(`Server Started on: ${PORT}`);
+    console.log(`Server Started on: ${PORT}`);
 });
