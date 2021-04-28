8ca71a51a63255ce477889f1028e458824436643
Saad Nadeem <xaad46@gmail.com>
2021-04-28 23:02:11 +0500

    Server Test v1

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+const PORT = process.env.PORT || 3030;
+
+io.on('connection', (socket) => {
+   
+    console.log('A User Connected');
+    socket.broadcast.emit("New User");
+
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected',userId);
+    });
+  
+    socket.on('disconnect', () => {
+        console.log('A User Disconnected');
+    });
+
+    socket.on('VideoPlayPause', function(pause){
+        socket.broadcast.emit('RecieveVid',pause);
+    });
+
+    socket.on('BarTimeUpdate', function(Time){
+        socket.broadcast.emit('BarTimeUpdate',Time);
+    });
+
+    socket.on('Chat Message', (Message) => {
+      // io.emit('LatestMessage', msg);
+      console.log("Message",Message);
+      socket.broadcast.emit('Chat Message', {"Message":Message});
+    });
+
+});
+
+server.listen(3030, () => {
+  console.log(`Server Started on: ${PORT}`);
+});
